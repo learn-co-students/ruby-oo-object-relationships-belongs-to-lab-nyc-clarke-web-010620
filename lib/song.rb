3571773f510d417cc053a5a3fc7237110e241cdd
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-13 00:28:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,12 @@
 class Song 
-  # attr_accessor :title 
+  attr_accessor :title, :artist
   
   def title=(title) 
     @title = title 
   end 
+  
+  def artist=(artist) 
+    @artist = artist 
+  end 
+  
 end 
